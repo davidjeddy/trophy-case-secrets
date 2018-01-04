11e972646da5605149985a0276c9e972d6d37559
David J Eddy <me@davidjeddy.com>
2018-01-03 22:08:44 -0500

    FIXED menu to work in Chrone 62+

--- tcs/menu.js
+++ tcs/menu.js
@@ -1,8 +1,8 @@
 //UDMv2.1.1
 var siSTR='';
-if (ie) { siSTR+='<script language=javascript src="' + baseHREF + 'menu_ie.js"></script>'; }
-if (ns4) { siSTR+='<script language=javascript src="' + baseHREF + 'menu_ns4.js"></script>'; }
-if (ns6||mz7) { siSTR+='<script language=javascript src="' + baseHREF + 'menu_ns6.js"></script>'; }
-if (op5&&!mac) { siSTR+='<script language=javascript src="' + baseHREF + 'menu_op5.js"></script>'; }
-document.write(siSTR);	
-	
+// if (ie) { siSTR='<script language=javascript src="' + baseHREF + 'menu_ie.js"></script>'; }
+// if (ns4) { siSTR='<script language=javascript src="' + baseHREF + 'menu_ns4.js"></script>'; }
+// if (ns6||mz7) { siSTR='<script language=javascript src="' + baseHREF + 'menu_ns6.js"></script>'; }
+// if (op5&&!mac) { siSTR='<script language=javascript src="' + baseHREF + 'menu_op5.js"></script>'; }
+if (siSTR === '') { siSTR='<script language=javascript src="./menu_ie.js"></script>'; }
+document.write(siSTR);
